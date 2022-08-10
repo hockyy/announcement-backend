83533b229a7b68dfc3ed67dd6511024605d47cfb
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-08-10 12:04:03 +0700

    Add cors

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-import json
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 announcement_dict = dict()
 @app.get("/")
